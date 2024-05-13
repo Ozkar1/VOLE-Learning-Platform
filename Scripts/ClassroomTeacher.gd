597101f5048cb4fa0f2f052c16188df5ddb31705
Hjarno1 <91209336+Hjarno1@users.noreply.github.com>
2024-05-13 17:46:10 +0200

    updated teacher classroom

--- Scripts/ClassroomTeacher.gd
+++ Scripts/ClassroomTeacher.gd
@@ -3,6 +3,9 @@ extends Node
 
 onready var http_request = $HTTPRequest
 onready var ClassroomsSelector = $ColorRect/OptionButton
+onready var ClassroomNameVBox = $ColorRect/ColorRect/ClassroomNameVBox
+onready var JoinCodeVBox = $ColorRect/ColorRect/JoinCodeVBox
+onready var ViewMoreVBOX = $ColorRect/ColorRect/ViewMoreVBox
 
 onready var classroomName
 
@@ -29,10 +32,25 @@ func _on_HTTPRequest_request_completed(result, response_code, headers, body):
 				var classroomName = "Classroom: " + item.ClassroomName
 				print("Classroom name received: " + item.ClassroomName)
 				ClassroomsSelector.add_item(classroomName)
+				add_classroom_info_label(item)
 		else:
 			print("Invalid response format: expected array")
 	else:
 		print("Failed to fetch classroom data:", response_code)
+
+func add_classroom_info_label(item):
+	if item.ClassroomName:
+		var classroom_name_label = Label.new()
+		classroom_name_label.text = item.ClassroomName
+		ClassroomNameVBox.add_child(classroom_name_label)
+		
+		var join_code_label = Label.new()
+		join_code_label.text = item.JoinCode
+		JoinCodeVBox.add_child(join_code_label)
+		
+		var view_button = Button.new()
+		view_button.text = "View"
+		ViewMoreVBOX.add_child(view_button)
 
 func _on_MenuBtn_pressed():
 	get_tree().change_scene("res://Scenes/MainMenu.tscn")
